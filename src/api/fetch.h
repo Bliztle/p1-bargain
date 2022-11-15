1c616c706e4d0b9531caf7f7686bdaab9a98cbb4
bliztle <asbjoern.r.e@gmail.com>
2022-11-15 23:59:11 +0100

    added directory and file for speaking with APIs
    Will be expanded later with files for reading and transforming data

---
+++ src/api/fetch.h
@@ -0,0 +1,1 @@
+void fetch_example();
--- src/main.c
+++ src/main.c
@@ -1,37 +1,10 @@
 #include <stdio.h>
 #include <curl/curl.h>
-
-void CurlTest();
+#include "api/fetch.h"
 
 int main()
 {
-    printf("Hello, World! - With a testottwo!\n");
-    printf("\nAs if we would write any tests...\n");
-    printf("\nI don't know what you're talking about\n");
-    CurlTest();
+    printf("Hello, World!\n");
+    fetch_example();
     return 0;
-}
-
-void CurlTest()
-{
-    CURL *curl;
-    CURLcode res;
-
-    curl = curl_easy_init();
-    if (curl)
-    {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
-        /* example.com is redirected, so we tell libcurl to follow redirection */
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-
-        /* Perform the request, res will get the return code */
-        res = curl_easy_perform(curl);
-        /* Check for errors */
-        if (res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
-
-        /* always cleanup */
-        curl_easy_cleanup(curl);
-    }
 }
