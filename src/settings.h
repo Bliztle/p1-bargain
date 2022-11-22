5576ac76dfec20dee9a834589cc5c056e37c0c9f
icecoldgold773 <mikkel.tygesen@gmail.com>
2022-11-22 16:44:36 +0100

    Created settings.c and settings.h

---
+++ src/settings.h
@@ -0,0 +1,5 @@
+// Returns if user inputs !q, otherwise redirects to other settings.
+void menu_settings();
+
+// Prints the current value of address, then changes it based on user input, and updates the coordinates in settings.conf.
+void menu_edit_address();
