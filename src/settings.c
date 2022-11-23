589a5c4ed023d697f704a935a3bfb70f16cd15f0
Kneckerino <mlrennekamp@gmail.com>
2022-11-23 16:39:46 +0100

    reintroduced switch-case for 1-4

--- src/settings.c
+++ src/settings.c
@@ -13,7 +13,36 @@ void menu_settings() {
 
     while (1) {
         print_menu();
+
+        scanf("%s", user_input);
         get_settings_input(user_input, __func__);
+
+        char first_char = user_input[0];
+
+        if (strlen(user_input) == 1) {
+            switch (first_char) {
+                case '1':
+                    printf("Edit savepath\n");
+                    break;
+
+                case '2':
+                    printf("Edit address\n");
+                    break;
+
+                case '3':
+                    printf("Edit distance\n");
+                    break;
+
+                case '4':
+                    printf("Edit deviation\n");
+                    break;
+
+                default:
+                    printf("Invalid input\n");
+                    break;
+            }
+        }
+
     }
 }
 
