3f45b212e4ec8afa5ea3a66a22d2b49ddfd78582
bliztle <asbjoern.r.e@gmail.com>
2022-11-24 10:56:09 +0100

    test setup and calc tests

---
+++ src/calc_test.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include "calc.h"
+#include <math.h>
+
+int main()
+{
+    assert(calc_degrees_to_radians(45) == M_PI_4);
+    assert(calc_degrees_to_radians(90) == M_PI_2);
+    assert(calc_degrees_to_radians(180) == M_PI);
+    return 0;
+}
