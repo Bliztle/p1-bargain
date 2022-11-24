5e689de81e58801766563bf0098e3becb81b9aec
Markus Dam <markus.dam123@gmail.com>
2022-11-24 15:01:30 +0100

    clean up config.c and config.h to add new function prototypes

--- src/config.h
+++ src/config.h
@@ -1,37 +1,27 @@
-typedef enum
-{
-    CONFIG_SUCCESS,
-    CONFIG_FAILLURE
-} config_status_e;
+#pragma once
+#include "api/parse.h"
 
-typedef enum
-{
-    SETTINGS,
-    TOKENS,
-    CONFIG_FILE_E_LENGTH
-} config_files_e;
+typedef char conf_address_t[50];
+// TODO: The typedefs for coordinates already exist in parse.h. Maybe they should be somewhere else?
+typedef double conf_deviance_t;
+typedef double conf_max_distance_t;
+typedef char conf_save_path_t[256];
 
-typedef enum
-{
-    ADDRESS,
-    COORDS,
-    DEVIANCE,
-    DISTANCE,
-    PATH,
-    CONFIG_SETTINGS_E_LENGTH
-} config_settings_e;
+typedef struct {
+    conf_address_t address;
+    coord_lat_t address_lat;
+    coord_lon_t address_lon;
+    conf_max_distance_t max_deviance;
+    conf_save_path_t shopping_list_save_path;
+} conf_settings_s;
 
-typedef enum
-{
-    SALLING,
-    COOP,
-    CONFIG_TOKENS_E_LENGTH
-} config_tokens_e;
-
-static const char *file_map[CONFIG_FILE_E_LENGTH];
-static const char *settings_map[CONFIG_SETTINGS_E_LENGTH];
-static const char *tokens_map[CONFIG_TOKENS_E_LENGTH];
-
-config_status_e config_write_value(char *file_path, char *key, char *value);
-config_status_e config_write_setting(config_settings_e key, char *value);
-config_status_e config_write_token(config_tokens_e key, char *value);
+/**
+ * Reads and parses the settings file stored on disk
+ * @returns the parsed config
+ */
+conf_settings_s conf_read_settings();
+/**
+ * Writes the given settings to the settings file on disk
+ * @params settings The settings to write
+ */
+void conf_write_settings(conf_settings_s settings);
