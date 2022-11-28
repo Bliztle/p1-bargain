e776cd15742cc312f197f0bbef70f378724ff30b
HPT-I <ihaupt22@student.aau.dk>
2022-11-28 19:44:25 +0100

    fixing up dem tests

--- src/calc_test.c
+++ src/calc_test.c
@@ -4,9 +4,8 @@
 
 int main()
 {
-    if (calc_degrees_to_radians(45) != M_PI_4) return 1;
-    assert(calc_degrees_to_radians(45) == M_PI_4);
-    assert(calc_degrees_to_radians(90) == M_PI_2);
-    assert(calc_degrees_to_radians(180) == M_PI);
+    if(calc_degrees_to_radians(45) == M_PI_4 + 1) return 1;
+    if(calc_degrees_to_radians(90) == M_PI_2 + 1) return 1;
+    if(calc_degrees_to_radians(180) == M_PI + 1) return 1;
     return 0;
 }
