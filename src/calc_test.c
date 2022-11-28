74a598c5174ad91809fb044d86902280c4969b21
HPT-I <ihaupt22@student.aau.dk>
2022-11-28 20:46:47 +0100

    testing ctest and actions

--- src/calc_test.c
+++ src/calc_test.c
@@ -4,8 +4,17 @@
 
 int main()
 {
-    assert(calc_degrees_to_radians(45) == M_PI_4);
-    assert(calc_degrees_to_radians(90) == M_PI_2);
-    assert(calc_degrees_to_radians(180) == M_PI);
+    if(calc_degrees_to_radians(45) != M_PI_4) {
+        printf("calc_degrees_to_radians(45) != M_PI_4 - Test failed\n");
+        exit(1);
+    }
+    if(calc_degrees_to_radians(90) != M_PI_2) {
+        printf("calc_degrees_to_radians(90) != M_PI_2 - Test failed\n");
+        exit(1);
+    }
+    if(calc_degrees_to_radians(180) == M_PI) {
+        printf("calc_degrees_to_radians(180) == M_PI - Test failed\n");
+        exit(1);
+    }
     return 0;
 }
