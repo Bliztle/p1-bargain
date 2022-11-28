4f6fdbcc2ce318e1806243341526de01c0830b5d
HPT-I <ihaupt22@student.aau.dk>
2022-11-28 19:33:28 +0100

    asdddd

--- src/calc_test.c
+++ src/calc_test.c
@@ -4,6 +4,7 @@
 
 int main()
 {
+    if (calc_degrees_to_radians(45) != M_PI_4) return 1;
     assert(calc_degrees_to_radians(45) == M_PI_4);
     assert(calc_degrees_to_radians(90) == M_PI_2);
     assert(calc_degrees_to_radians(180) == M_PI);
