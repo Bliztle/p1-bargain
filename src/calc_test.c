c8135ab28d45b8905dc9f11c833640ed42849a9a
HPT-I <ihaupt22@student.aau.dk>
2022-11-28 22:23:29 +0100

    qwera

--- src/calc_test.c
+++ src/calc_test.c
@@ -4,7 +4,8 @@
 
 int main()
 {
-    assert(calc_degrees_to_radians(45) != M_PI_4);
+    int pass = (calc_degrees_to_radians(45) != M_PI_4);
+    assert(pass);
     assert(calc_degrees_to_radians(90) == M_PI_2);
     assert(calc_degrees_to_radians(180) == M_PI);
     return 0;
