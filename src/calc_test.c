396c40f03bda675483f47384641949b0e77b4044
HPT-I <ihaupt22@student.aau.dk>
2022-11-28 21:45:32 +0100

    asd

--- src/calc_test.c
+++ src/calc_test.c
@@ -1,20 +1,11 @@
-#include <assert.h>
+#include "assert.h"
 #include "calc.h"
 #include <math.h>
 
 int main()
 {
-    if(calc_degrees_to_radians(45) != M_PI_4) {
-        printf("calc_degrees_to_radians(45) != M_PI_4 - Test failed\n");
-        exit(1);
-    }
-    if(calc_degrees_to_radians(90) != M_PI_2) {
-        printf("calc_degrees_to_radians(90) != M_PI_2 - Test failed\n");
-        exit(1);
-    }
-    if(calc_degrees_to_radians(180) == M_PI) {
-        printf("calc_degrees_to_radians(180) == M_PI - Test failed\n");
-        exit(1);
-    }
+    assert(calc_degrees_to_radians(45) == M_PI_4);
+    assert(calc_degrees_to_radians(90) == M_PI_2);
+    assert(calc_degrees_to_radians(180) == M_PI);
     return 0;
 }
