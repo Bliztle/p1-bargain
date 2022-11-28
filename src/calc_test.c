bd538d96f9aecbfaa1b648343277a08c9bb2670d
HPT-I <ihaupt22@student.aau.dk>
2022-11-28 22:17:41 +0100

    asgb

--- src/calc_test.c
+++ src/calc_test.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    assert(calc_degrees_to_radians(45) == M_PI_4);
+    assert(calc_degrees_to_radians(45) != M_PI_4);
     assert(calc_degrees_to_radians(90) == M_PI_2);
     assert(calc_degrees_to_radians(180) == M_PI);
     return 0;
