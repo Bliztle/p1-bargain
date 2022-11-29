9bc18d7b841d9cf265ffdc6d9f3004210f9525e3
Kneckerino <mlrennekamp@gmail.com>
2022-11-29 14:54:46 +0100

    Implemented the reading of path and distance

--- src/config.c
+++ src/config.c
@@ -9,31 +9,42 @@ static const char* SETTINGS_PATH = "settings.conf";
 static const char* TOKENS_PATH = "tokens.conf";
 
 int main(void) {
-    
-    return 0;
+    conf_read_settings();
+
 }
 
 conf_settings_s conf_read_settings() {
     FILE * config_file;
-    char * path = "NEEDS PATH";
+    char * path = "C:\\Users\\mlren\\CLionProjects\\p1-bargain\\src\\settings.conf";
     config_file = fopen(path, "r");
     if (config_file == NULL) {
         perror("File does not exist in this path");
         exit(EXIT_FAILURE);
     }
 
-    conf_settings_s read_file;
+    conf_settings_s read_settings;
+    config_parse_path(config_file, read_settings.shopping_list_save_path);
+    config_parse_distance(config_file, &read_settings.max_distance);
 
-    strcpy(read_file.shopping_list_save_path, config_parse_path());
+    printf("%s", read_settings.shopping_list_save_path);
+    printf("%dm", read_settings.max_distance);
 
-
-    return read_file;
+    fclose(config_file);
+    return read_settings;
 }
 
 void config_write_settings(conf_settings_s settings) {
 
 }
 
-char * config_parse_path() {
+void config_parse_path(FILE * config_file, conf_save_path_t save_path) {
+    int size = sizeof(conf_save_path_t) / sizeof(char);
+    char str[size];
+    fgets(str, size, config_file);
+    char * ptr = strstr(str, " ") + 1; // Shift the pointer by 1 to not get whitespace in front
+    strcpy(save_path, ptr);
+}
 
+void config_parse_distance(FILE * config_file, conf_max_distance_t * distance) {
+    fscanf(config_file, "%*s %d", distance);
 }
--- src/config.h
+++ src/config.h
@@ -11,7 +11,8 @@ typedef struct {
     conf_address_t address;
     coord_lat_t address_lat;
     coord_lon_t address_lon;
-    conf_max_distance_t max_deviance;
+    conf_deviance_t deviance;
+    conf_max_distance_t max_distance;
     conf_save_path_t shopping_list_save_path;
 } conf_settings_s;
 
@@ -28,7 +29,15 @@ conf_settings_s conf_read_settings();
 void conf_write_settings(conf_settings_s settings);
 
 /**
- *
- * @returns pointer to the first character of the string of the path
+ * Takes file and reads save path onto char[]
+ * @param config_file The file to read from
+ * @param save_path The value to save to
  */
-char * config_parse_path();
+void config_parse_path(FILE * config_file, conf_save_path_t save_path);
+
+/**
+ * Takes file and reads distance onto char[]
+ * @param config_file The file to read from
+ * @param distance The value to save to
+ */
+void config_parse_distance(FILE * config_file, conf_max_distance_t * distance);
