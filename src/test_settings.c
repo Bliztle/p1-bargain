7a3c54d35452fd9c880a224199323fc6a38a11e0
Mikkel Tygesen <83021047+icecoldgold773@users.noreply.github.com>
2022-11-30 15:50:40 +0000

    Added comment

--- src/test_settings.c
+++ src/test_settings.c
@@ -2,12 +2,14 @@
 #include "settings.h"
 
 int main(void) {
-    char* input1 = "C:\\Users\\Mikkel Tygesen\\Desktop\\save.txt";
+    // Path test depends on the system it is tested on
+
+    //char* input1 = "C:\\User\\Desktop\\file.txt";
     char* input2 = "Selma Lagerløfs Vej 300 Aalborg 9220";
     char* input3 = "10";
     char* input4 = "0.5";
 
-    assert(settings_validate(input1, PATH));
+    //assert(settings_validate(input1, PATH));
     assert(settings_validate(input2, ADDRESS));
     assert(settings_validate(input3, DISTANCE));
     assert(settings_validate(input4, DEVIATION));
