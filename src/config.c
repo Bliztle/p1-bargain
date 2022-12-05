99a5e043e16b958b10d2b42883205456acd56922
Mikkel Tygesen <83021047+icecoldgold773@users.noreply.github.com>
2022-12-05 11:15:36 +0000

    Fixed CMakeList

--- src/config.c
+++ src/config.c
@@ -7,30 +7,35 @@
 
 //static const char* TOKENS_PATH = "tokens.conf";
 
-int conf_read_settings(conf_settings_s *settings) {
-    FILE * config_file = fopen(SETTINGS_PATH, "r");
+int conf_read_settings(conf_settings_s *conf_settings) {
+    FILE *config_file = fopen(SETTINGS_PATH, "r");
+
     if (config_file == NULL) {
-        perror("File does not exist in this path");
+        perror("Error");
+
         return 0;
     }
 
-    conf_settings_s read_settings;
-
-    fscanf(config_file, "%*s %s", settings->shopping_list_save_path);
-    fscanf(config_file, "%*s %d", &settings->max_distance);
-    fscanf(config_file, "%*s %[^\n]s", settings->address);
+    fscanf(config_file, "%*s %s", conf_settings->shopping_list_save_path);
+    fscanf(config_file, "%*s %d", &conf_settings->max_distance);
+    fscanf(config_file, "%*s %[^\n]s", conf_settings->address);
 
     char lat_temp[20], lon_temp[20];
+
     fscanf(config_file, "%*s %s %s", lat_temp, lon_temp);
+
     char *endptr;
-    settings->address_lat = strtod(lat_temp, &endptr);
-    settings->address_lon = strtod(lon_temp, &endptr);
+    conf_settings->address_lat = strtod(lat_temp, &endptr);
+    conf_settings->address_lon = strtod(lon_temp, &endptr);
 
     char dev_temp[20];
+
     fscanf(config_file, "%*s %s", dev_temp);
-    settings->deviance = strtod(dev_temp, &endptr);
+
+    conf_settings->deviance = strtod(dev_temp, &endptr);
 
     fclose(config_file);
+
     return 1;
 }
 
