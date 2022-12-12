8642b8ba1bd8bf88f6929c04118ebdc6e62e8a87
Mikkel Tygesen <83021047+icecoldgold773@users.noreply.github.com>
2022-12-12 08:32:47 +0000

    Fixed pointer

--- src/settings.c
+++ src/settings.c
@@ -109,11 +109,11 @@ void settings_edit(conf_settings_s *settings, int setting) { // Edit the given s
                     double lat;
                     double lon;
 
-                    parse_coordinates(lat, lon, &raw_coordinates);
+                    parse_coordinates(&lat, &lon, &raw_coordinates);
 
                     settings->address_lat = lat;
                     settings->address_lon = lon;
-                    
+
                     break;
 
                 case DISTANCE:
