623aa7caa81e68fcb416bfa34e84ab5256f7b34c
Mikkel Tygesen <83021047+icecoldgold773@users.noreply.github.com>
2022-12-14 14:51:09 +0000

    Equal to main branch

--- src/main.c
+++ src/main.c
@@ -6,8 +6,7 @@ int main(int argc, char **args)
 {
     CURRENT_DIRECTORY = args[0];
     // TODO: Run conf_setup() to setup the config file.
-    
-    menu_settings();
+    bargain_run_bargain();
 
     return 0;
 }
